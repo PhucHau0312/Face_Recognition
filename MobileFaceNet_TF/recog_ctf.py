d66b8d589735dc81ad9d1cd8fbafebca8ca64740
PhucHau0312 <105101462+PhucHau0312@users.noreply.github.com>
2022-10-29 21:25:06 +0700

    Update recog_ctf.py

--- MobileFaceNet_TF/recog_ctf.py
+++ MobileFaceNet_TF/recog_ctf.py
@@ -61,9 +61,9 @@ def parse_arguments(argv):
     parser.add_argument('--model', type=str,
                         help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file',
                         default='./arch/pretrained_model')
-	parser.add_argument('--emb_path', default='/home/hau/Desktop/FaceRecognition/MobileFaceNet_TF/embeddings/', help='the embeddings path')
+    parser.add_argument('--emb_path', default='/home/hau/Desktop/FaceRecognition/MobileFaceNet_TF/embeddings/', help='the embeddings path')
     parser.add_argument('--image_size', default=[112, 112], help='the image size')
-	parser.add_argument('--threshold', default=0.8, help='the threshold')
+    parser.add_argument('--threshold', default=0.8, help='the threshold')
     return parser.parse_args(argv)
 
 
